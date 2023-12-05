f7becb37286c31c86532f495847551be9540cf80
TNealy <66037350+JunNealy@users.noreply.github.com>
2023-12-05 16:11:06 -0500

    Style Nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styles from '../styles';
+import { discount, robot } from '../assets';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className={`felx md:flex-row flex col ${styles.paddingY}`}
+    ></section>
+  );
 };
 
 export default Hero;
